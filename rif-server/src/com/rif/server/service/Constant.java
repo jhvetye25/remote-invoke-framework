164ead2d16bae10d6ef290f6ae1958835f1a1d7d
jxta.liu <jxta.liu@gmail.com>
2011-08-26 17:35:18 +0000

    Update:add notes

--- rif-server/src/com/rif/server/service/Constant.java
+++ rif-server/src/com/rif/server/service/Constant.java
@@ -1,5 +1,10 @@
 package com.rif.server.service;
 
+/**
+ * 
+ * @author bruce.liu (mailto:jxta.liu@gmail.com)
+ *
+ */
 public class Constant {
 	public static final String IMPLEMENT_TYPE_POJO = "pojo";
 	public static final String IMPLEMENT_TYPE_SPRING = "spring";
